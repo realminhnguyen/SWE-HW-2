300025751c02a511048e356bb095097dc0c934ad
realminhnguyen <real.minh.n@gmail.com>
2022-10-05 12:44:39 -0500

    Initial Commit

---
+++ SWE HW 2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SWE HW 2
+//
+//  Created by Minh on 10/5/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
